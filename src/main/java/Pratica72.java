c4b2bbc56c2fe3153c1415dda60afcac8f77887f
Jhonatan <Jhonatan@192.168.107.89>
2015-02-09 16:27:57 -0200

    First commit

---
+++ src/main/java/Pratica72.java
@@ -0,0 +1,26 @@
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import utfpr.ct.dainf.if62c.pratica.ContadorPalavras;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Jhonatan
+ */
+public class Pratica72 {
+    public static void main(String[] args) throws FileNotFoundException, IOException 
+    {
+        String end = "C:/Documents and Settings/Jhonatan/Desktop/teste.txt";
+        FileReader fr = new FileReader(end);
+        ContadorPalavras CP = new ContadorPalavras(fr);
+        
+        HashMap<String, Integer> map = CP.getPalavras();
+    }
+}
---
+++ src/main/java/utfpr/ct/dainf/if62c/pratica/ContadorPalavras.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utfpr.ct.dainf.if62c.pratica;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
+/**
+ *
+ * @author Jhonatan
+ */
+public class ContadorPalavras {
+    
+    private BufferedReader reader;
+    private HashMap<String, Integer> map = new HashMap<>();
+    private String line;
+    
+    public ContadorPalavras(FileReader in) {
+        reader = new BufferedReader(in);
+    }
+    
+    public HashMap<String, Integer> getPalavras() throws IOException
+    {
+        line = reader.readLine();
+        while(line!=null)
+        {
+            String []trecho = line.split(" ");
+            for(String s: trecho)
+            {
+                if(map.containsKey(s))
+                    map.put(s, map.get(s)+ 1);
+                else map.put(s, 1);
+            }
+            line = reader.readLine();
+        }
+        return map;
+    }
+}
